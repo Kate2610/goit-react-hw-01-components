513537d7babe7ef35327fd177eeb0582dcb93249
Kate2610 <yekaterina.anisimova@gmail.com>
2023-02-28 19:59:12 +0200

    new

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr style={{ backgroundColor: 'turquoise' }}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} style={{ backgroundColor: 'rgba(211, 211, 211, 0.2)' }}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
